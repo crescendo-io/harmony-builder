533a39e725c02593f2a4ca4ed490e0e407cee7a2
Bryan VIDAL <b.vidal@lonsdale.fr>
2024-07-09 09:58:17 +0200

    Maj 9 juillet 2024

--- web/wp-content/themes/harmony-builder/template-parts/general/bloc-button.php
+++ web/wp-content/themes/harmony-builder/template-parts/general/bloc-button.php
@@ -1,7 +1,9 @@
 <?php
     $buttons = get_sub_field('buttons_enable');
     $buttons_1 = get_sub_field('buttons_1');
+    $buttons_1_class = get_sub_field('buttons_1_class');
     $buttons_2 = get_sub_field('buttons_2');
+    $buttons_2_class = get_sub_field('buttons_2_class');
 
     if($buttons):
 ?>
@@ -9,12 +11,12 @@
 
 <div class="container-buttons">
     <?php if($buttons_1): ;?>
-        <a href="<?= $buttons_1['url']; ?>" target="<?= $buttons_1['target']; ?>" class="button primary">
+        <a href="<?= $buttons_1['url']; ?>" target="<?= $buttons_1['target']; ?>" class="button primary <?= ($buttons_1_class) ? $buttons_1_class : ''; ?>">
             <?= $buttons_1['title']; ?>
         </a>
     <?php endif; ?>
     <?php if($buttons_2): ;?>
-        <a href="<?= $buttons_2['url']; ?>" target="<?= $buttons_2['target']; ?>" class="button secondary">
+        <a href="<?= $buttons_2['url']; ?>" target="<?= $buttons_2['target']; ?>" class="button secondary <?= ($buttons_2_class) ? $buttons_2_class : ''; ?>">
             <?= $buttons_2['title']; ?>
         </a>
     <?php endif; ?>
